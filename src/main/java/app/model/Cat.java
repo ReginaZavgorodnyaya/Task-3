ad24fcb925d2a8f028d79ccd62342dd86b684700
ReginaZavgorodnyaya <zavgorodnyayaregina@gmail.com>
2024-06-12 16:28:09 +0300

    result task-3

--- src/main/java/app/model/Cat.java
+++ src/main/java/app/model/Cat.java
@@ -1,11 +1,16 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Cat extends Animal {
+
     @Override
     public String toString() {
         return "Im a Cat";
     }
 }
+
+
